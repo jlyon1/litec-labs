34e6af910556bdbf0d94c183b966bc7d88d8848c
unknown <akhilcjacob@gmail.com>
2017-03-23 12:17:40 -0400

    fixed hw 10 bugs

--- LAB3/HW10/hw10.c
+++ LAB3/HW10/hw10.c
@@ -27,9 +27,10 @@ unsigned int val;
 int count;
 char input;
 unsigned int ranger = 0;
+unsigned int heading = 0;
 unsigned int flag = 0;
 unsigned int compFlag = 0;
-unsigned int heading = 0;
+
 
 //-----------------------------------------------------------------------------
 // Main Function
@@ -45,7 +46,7 @@ void main(void)
   SMB_init();
 
   count = 0;
-	//i2c_start();
+  //i2c_start();
   while(1){
 
     if(flag == 4){
@@ -72,7 +73,7 @@ void main(void)
       unsigned int Hi_Low_Bytes = 0;
       unsigned char addr = 0xC0; // the address of the sensor, 0xC0 for the compass
       unsigned char Data[2]; // Data is an array with a length of 2
-      unsigned int heading; // the heading returned in degrees between 0 and 3599
+      //unsigned int heading; // the heading returned in degrees between 0 and 3599
       i2c_read_data(addr, 0, Data, 1); // read One byte, starting at reg o
       Software_Version=(Data[0]); //combine the two values
       i2c_read_data(addr, 12, Data, 2); // read One byte, starting at reg o
@@ -81,7 +82,7 @@ void main(void)
     //Print here to not slow down
     if(count % 4 == 0){
       printf("Range %u\r\n", ranger);
-      printf("Heading %u\r\n", heading)
+      printf("Heading %u\r\n", heading);
     }
   }
 }
@@ -95,8 +96,8 @@ void main(void)
 void Port_Init()
 {
     P1MDOUT |= 0x04;  //set output pin for CEX2 in push-pull mode
-	P0MDOUT &= 0b11110011;
-	P0 |= ~0b11110011;
+  P0MDOUT &= 0b11110011;
+  P0 |= ~0b11110011;
 }
 
 //-----------------------------------------------------------------------------
@@ -120,12 +121,12 @@ void XBR0_Init()
 void PCA_Init(void)
 {
     PCA0MD = 0b01110001;//Set to sysclock/12
-	//PCA0MD = 0x
+  //PCA0MD = 0x
     PCA0CPM0 = 0xC2;
-	PCA0CPM2 = 0xC2;
+  PCA0CPM2 = 0xC2;
     PCA0CN = 0x40;
     EIE1 |= 0b00001000;
-	EA = 1;
+  EA = 1;
 
 }
 
@@ -138,13 +139,13 @@ void PCA_Init(void)
 void PCA_ISR ( void ) __interrupt 9
 {
 
-	if(CF){
-		CF = 0;
-		PCA0 = 28670;
-		flag += 1;
+  if(CF){
+    CF = 0;
+    PCA0 = 28670;
+    flag += 1;
     compFlag += 1;
-		count += 1;
-	}
+    count += 1;
+  }
 
 }
 
@@ -152,48 +153,48 @@ void Steering_Servo()
 {
     char input;
     //wait for a key to be pressed
-	input = getchar();
+  input = getchar();
     if(input == 'l')  // single character input to decrease the pulsewidth
     {
-		printf("\r\nturn left\r\n");
+    printf("\r\nturn left\r\n");
 
-		PW -= 10;
+    PW -= 10;
         if(PW < PW_MIN){  // check if less than pulsewidth minimum
-        	PW = PW_MIN;    // set SERVO_PW to a minimum value
-		}
+          PW = PW_MIN;    // set SERVO_PW to a minimum value
+    }
     }
     else if(input == 'r')  // single character input to increase the pulsewidth
     {
-       	printf("\r\nturn right\r\n");
+        printf("\r\nturn right\r\n");
 
-		PW += 10;
+    PW += 10;
         if(PW > PW_MAX)  // check if pulsewidth maximum exceeded
-        	PW = PW_MAX;     // set PW to a maximum value
+          PW = PW_MAX;     // set PW to a maximum value
     }else if(input == 'b')  // single character input to decrease the pulsewidth
     {
-		printf("\r\ndecrease speed \r\n");
+    printf("\r\ndecrease speed \r\n");
 
-		DRIVE_PW -= 10;
+    DRIVE_PW -= 10;
 
     }
     else if(input == 'f')  // single character input to increase the pulsewidth
     {
-       	printf("\r\nincrease speed\r\n");
+        printf("\r\nincrease speed\r\n");
 
-		DRIVE_PW += 10;
+    DRIVE_PW += 10;
 
     }
 
     printf("PW: %u\n", PW);
-	printf("DRIVE_PW: %u\n", DRIVE_PW);
+  printf("DRIVE_PW: %u\n", DRIVE_PW);
     PCA0CP0 = 0xFFFF - PW;
-	PCA0CP2 = 0xFFFF - DRIVE_PW;
+  PCA0CP2 = 0xFFFF - DRIVE_PW;
 
 }
 
 void SMB_init(void){
-	SMB0CR = 0x93;
-	ENSMB=1;
+  SMB0CR = 0x93;
+  ENSMB=1;
 }
 
 unsigned int ReadCompass(void){
